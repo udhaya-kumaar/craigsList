06b3eda3600e425c8243dfbb93d0234dfce01a10
Manish Sivanandan <manishsivanandan@gmail.com>
2015-08-21 22:29:02 +0530

    loop based fetch

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
+var url = "http://bakersfield.craigslist.org/";
+var base_url = url+"search/vac?query=view";
 
 /**
  * Get base url from command line.
  * Format : node server.js <url>
  */
-var base_url = process.argv[2];
+var limit = process.argv[2] || 100000;
 var Data = [];
 
 /* Request for content */
@@ -22,8 +24,34 @@ request(base_url,function(err,response,body){
 				url:d.attribs.href
 			});
 		});
-		/* Write to file */
-		fs.writeFileSync('final_data.json',JSON.stringify(Data));
+		/* Start looping through the links */
+		var iterator = 0;
+		var Store = [];
+		function recur(){
+			if(iterator == limit){
+				fs.writeFileSync('final_data.json',JSON.stringify(Store));
+				console.log("Done!");
+			}
+			else{
+				request(url+Data[0].url,function(e,r,b){
+					if(r && r.statusCode == 200){
+						var $ = cheerio.load(b);
+						var map = $('#map');
+						var item = {
+							title:Data[0].name,
+							text:$('#postingbody').text(),
+							map:map && map[0]? [map[0].attribs['data-latitude'],map[0].attribs['data-longitude']] : []
+						};
+						Store.push(item);
+						Data.splice(0,1);
+						console.log((iterator+1)+" done...");
+						iterator++;
+						recur();
+					}
+				})
+			}
+		}
+		recur();
 	}
 	else console.log("Error occured",err);
 });
